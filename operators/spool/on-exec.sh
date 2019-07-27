706c821f8d0f0c55e2434ad1bce25ad105b446ed
Anish Thomas <anish.thomas@evolvussolutions.com>
2019-07-27 15:16:48 +0530

    spelling errors

--- operators/spool/on-exec.sh
+++ operators/spool/on-exec.sh
@@ -1,4 +1,4 @@
 echo "Executing spool on-exec"
 
-source $PRECISION100_OPERATOR_FOLDER/spool/conf/.operator.env.sh
+source $PRECISION100_OPERATORS_FOLDER/spool/conf/.operator.env.sh
 export PRECISION100_OPERATOR_SPOOL_FOLDER
