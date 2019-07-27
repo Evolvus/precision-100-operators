f7b85291a0a2300f62270cdfcee840324ddfba36
Anish Thomas <anish.thomas@evolvussolutions.com>
2019-07-27 16:12:40 +0530

    minor errors

--- operators/spool/on-init-exec.sh
+++ operators/spool/on-init-exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Executing spool on-init-exec"
 
-source $PRECISION100_OPERATOR_FOLDER/spool/conf/.operator.env.sh
+source $PRECISION100_OPERATORS_FOLDER/spool/conf/.operator.env.sh
 mkdir -p $PRECISION100_OPERATOR_SPOOL_FOLDER
