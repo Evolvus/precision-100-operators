982737eca7caec577d3ea31249f20303884b669c
Anish Thomas <anish.thomas@evolvussolutions.com>
2019-09-26 18:15:20 +0530

    corrected bugs

--- operators/masking-mapper/bin/masking-mapper-i-table.sh
+++ operators/masking-mapper/bin/masking-mapper-i-table.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-TABLE_NAME_PREFIX=${DEFAULT_TABLE_NAME_PREFIX:-O}
-FILE_NAME_SUFFIX=${DEFAULT_FILE_NAME_SUFFIX:-csv}
-
 MAP_FILE_DELIMITER=${DEFAULT_MAP_FILE_DELIMITER:-~}
 
-FILE_NAME=$1
-MAPPED_TABLE_NAME="${TABLE_NAME_PREFIX}_${TABLE_NAME}"
+SOURCE_FILE=$1
 
 echo "DELETE FROM MASKING_MAPPER;"
 
 counter=0
-while IFS='~' read -r schema_name table_name column_name phase mapping_method update_where;
+while IFS='~' read -r schema_name table_name column_name phase mapping_method update_where enabled_flag;
 do
   if [[ -z "$column_name" ]]; then
      continue;
@@ -20,5 +16,8 @@ do
     counter=$counter+1;
     continue;
   fi
-  echo "INSERT INTO MASKING_MAPPER ( SCHEMA_NAME, TABLE_NAME, COLUMN_NAME, PHASE, MAPPING_METHOD, UPDATE_WHERE ) VALUES ( UPPER('$schema_name'), UPPER('${table_name}'), UPPER('$column_name'), UPPER('$phase'), UPPER('$mapping_method'), UPPER('$update_where') );"
+  if [[ -z $enabled_flag ]]; then
+    enabled_flag='Y'
+  fi
+  echo "INSERT INTO MASKING_MAPPER ( SCHEMA_NAME, TABLE_NAME, COLUMN_NAME, PHASE, OPERATION_METHOD, UPDATE_WHERE, ENABLED_FLAG ) VALUES ( UPPER('$schema_name'), UPPER('${table_name}'), UPPER('$column_name'), UPPER('$phase'), UPPER('$mapping_method'), UPPER('$update_where'), UPPER('$enabled_flag' );"
 done < <(cat ${SOURCE_FILE} | tr '\t' '~' | tr -d '\r' | grep .)
--- operators/masking-mapper/on-exec.sh
+++ operators/masking-mapper/on-exec.sh
@@ -1,1 +1,4 @@
 echo "Executing masking-mapper on-exec"
+
+source $PRECISION100_OPERATORS_FOLDER/masking-mapper/conf/.operator.env.sh
+export MASKING_MAPPER_WORK_FOLDER;
