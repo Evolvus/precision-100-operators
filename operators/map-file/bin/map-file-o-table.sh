8d0977b4b0c1a573161731f5f61cd77424485a8f
Anish Thomas <anish.thomas@evolvussolutions.com>
2019-09-29 09:50:18 +0530

    added extra columns

--- operators/map-file/bin/map-file-o-table.sh
+++ operators/map-file/bin/map-file-o-table.sh
@@ -12,47 +12,38 @@ MAP_FILE_DELIMITER=${DEFAULT_MAP_FILE_DELIMITER:-~}
 TABLE_NAME=$1
 SOURCE_FILE=$2
 
-echo "DROP TABLE ${TABLE_NAME_PREFIX}_${TABLE_NAME};"
-echo "CREATE TABLE ${TABLE_NAME_PREFIX}_${TABLE_NAME} ("
-counter=0
-while IFS='~' read -r column_name data_type max_length mapping_code mapping_value;
-do
-  if [[ -z "$column_name" ]]; then
-    continue;
-  fi
-  if [[ counter -eq 0 ]]; then
+function define_table() {
+  upper_case_table_name=$(echo "${1}_${2}" | tr '[:lower:]' '[:upper:]')
+  echo "DROP TABLE ${upper_case_table_name};"
+  echo "CREATE TABLE ${upper_case_table_name} ("
+  counter=0
+  while IFS='~' read -r column_name old_column_name data_type max_length mapping_code mapping_value;
+  do
+    if [[ -z "$column_name" ]]; then
+      continue;
+    fi
+    if [[ counter -eq 0 ]]; then
+      counter=$counter+1;
+      continue;
+    fi
+    if [[ counter -eq 1 ]]; then
+      counter=$counter+1;
+      get_column_definition "$column_name" $max_length
+      continue;
+    fi
     counter=$counter+1;
-    continue;
-  fi
-  if [[ counter -eq 1 ]]; then
-    counter=$counter+1;
-    echo "  $column_name $COLUMN_DATA_TYPE"
-    continue;
-  fi
-  counter=$counter+1;
-  echo ", $column_name $COLUMN_DATA_TYPE"
-done < <(cat ${SOURCE_FILE} | tr '\t' '~' | tr -d '\r' | grep .)
-echo ");"
+    echo ", $(get_column_definition "$column_name" $max_length)"
+  done < <(cat ${SOURCE_FILE} | tr '\t' '~' | tr -d '\r' | grep .)
+  echo ");"
+}
 
-# Create the r tables
-echo "DROP TABLE ${REVERSE_TABLE_NAME_PREFIX}_${TABLE_NAME};"
-echo "CREATE TABLE ${REVERSE_TABLE_NAME_PREFIX}_${TABLE_NAME} ("
-counter=0
-while IFS='~' read -r column_name data_type max_length mapping_code mapping_value;
-do
-  if [[ -z "$column_name" ]]; then
-    continue;
-  fi
-  if [[ counter -eq 0 ]]; then
-    counter=$counter+1;
-    continue;
-  fi
-  if [[ counter -eq 1 ]]; then
-    counter=$counter+1;
-    echo "  $column_name $COLUMN_DATA_TYPE"
-    continue;
-  fi
-  counter=$counter+1;
-  echo ", $column_name $COLUMN_DATA_TYPE"
-done < <(cat ${SOURCE_FILE} | tr '\t' '~' | tr -d '\r' | grep .)
-echo ");"
+function get_column_definition() {
+  v_column_name=$(echo ${1:0:30} | tr '[:lower:]' '[:upper:]')
+  v_data_type="NVARCHAR2"
+  v_column_length=${2:-2000}
+
+  echo " $v_column_name $v_data_type($v_column_length)"
+}
+
+define_table ${TABLE_NAME_PREFIX} ${TABLE_NAME}
+define_table ${REVERSE_TABLE_NAME_PREFIX} ${TABLE_NAME}
