ee4c9277a6663f137e92a853b30e529b66b7cc13
anish thomas alex <anishthomas@anishs-MacBook-Pro.local>
2025-02-13 12:02:20 +0800

    added git repo operator

---
+++ app/src/p100/operator/repo/git_repo_operator.py
@@ -0,0 +1,48 @@
+import os, logging
+import subprocess
+
+PARAM_LOCAL_URL = "local_url"
+PARAM_REMOTE_URL = "remote_url"
+PARAM_EVENT = "event"
+PARAM_OPERATION_MODE = "operation_mode"
+SUPPORTED_EVENTS = {"checkout", "refresh", "branch"}
+
+logger = logging.getLogger(__name__)
+
+def execute(**context):
+    # Get the context
+    local_url = context[PARAM_LOCAL_URL]
+    remote_url = context[PARAM_REMOTE_URL]
+    event = context[PARAM_EVENT].lower()
+    operation_mode = context[PARAM_OPERATION_MODE]
+
+    logger.info(
+        f"Copying files from {remote_url} to {local_url} for event: {event} and operation mode: {operation_mode}"
+    )
+
+    if event not in SUPPORTED_EVENTS:
+        logger.error(
+            f"Invalid event: {event}. Supported events are: {', '.join(SUPPORTED_EVENTS)}"
+        )
+        return -1
+
+    if os.path.isdir(local_url) and os.listdir(local_url):
+        logger.error(f"Local repository exists and is not empty: {local_url}")
+        return -3
+
+    if event == "checkout":
+        try:
+            subprocess.run(["git", "clone", remote_url, local_url], check=True)
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error cloning repository from {remote_url} to {local_url}: {e}")
+            return -4
+
+    elif event == "refresh":
+        try:
+            subprocess.run(["git", "-C", local_url, "pull"], check=True)
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error pulling updates in repository at {local_url}: {e}")
+            return -4
+
+    logger.info(f"Successfully handled event: {event} for repository at {local_url}")
+    return 0
