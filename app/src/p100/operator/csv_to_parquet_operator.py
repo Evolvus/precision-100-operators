fd7c23c07e801293338c902682ea990d82a5492e
ennovatenow <anish.thomas@evolvussolutions.com>
2025-02-21 01:05:05 +0800

    few more operators

---
+++ app/src/p100/operator/csv_to_parquet_operator.py
@@ -0,0 +1,38 @@
+import logging
+from .cmdline_operator import execute_cmd
+
+
+logger = logging.getLogger(__name__)
+
+def execute(line, **context):
+    logger.info(f"Executing {__name__} operator {line}")
+    logger.debug(
+        f"Executing {__name__} operator with {line} parameters and context: {context}"
+    )
+
+    layout_operator_lookup = context.get("__LAYOUT_OPERATOR__")
+    if not layout_operator_lookup:
+        logger.error("Layout operator not provided in context")
+        return -1
+
+    dataflow = context.get("__DATAFLOW__")
+    container = context.get("__CONTAINER__")
+
+    # Get the scsv file name
+    csv_file_name = layout_operator_lookup(
+        dataflow, container, line.get("__PARAM0__"), **context
+    )
+
+    parquet_file_name = layout_operator_lookup(
+        dataflow, container, line.get("__PARAM1__"), **context
+    )
+
+    script_name = f"copy (select * from read_csv('{csv_file_name}')) to '{parquet_file_name}' (format parquet)"
+
+    # Construct the command
+    command = [
+        "duckdb",
+        "-c",
+        script_name,
+    ]
+    return execute_cmd(command)
